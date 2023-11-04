c747d42fb3fcf7e4b12b3e9b5f69ffe1bccbedf3
suba <84484832+suba327777@users.noreply.github.com>
2023-11-05 03:40:07 +0900

    add: get diagnostic info at the cursor position fn

--- lua/yank_diag/module.lua
+++ lua/yank_diag/module.lua
@@ -1,7 +1,20 @@
+local api = vim.api
+local diagnostic = vim.diagnostic
+
 local M = {}
 
+local function get_cursor_diagnostic_message()
+  local cur_buf = api.nvim_get_current_buf()
+  local line, col = unpack(api.nvim_win_get_cursor(0))
+  local cursor_diagnostic = diagnostic.get(cur_buf, { lnum = line - 1, col = col })
+  local diagnostic_mesgage = cursor_diagnostic[1].message
+
+  print(diagnostic_mesgage)
+  return diagnostic_mesgage
+end
+
 M.yank_diag = function()
-  return print("hello world")
+  get_cursor_diagnostic_message()
 end
 
 return M
