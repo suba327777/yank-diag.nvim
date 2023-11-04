2c4e7450d23deb914368a5a28e9fb37f16dee7c6
suba <84484832+suba327777@users.noreply.github.com>
2023-11-05 03:57:08 +0900

    add: handling when get_cursor_diagnostic_message is nil

--- lua/yank_diag/module.lua
+++ lua/yank_diag/module.lua
@@ -7,14 +7,16 @@ local function get_cursor_diagnostic_message()
   local cur_buf = api.nvim_get_current_buf()
   local line, col = unpack(api.nvim_win_get_cursor(0))
   local cursor_diagnostic = diagnostic.get(cur_buf, { lnum = line - 1, col = col })
-  local diagnostic_mesgage = cursor_diagnostic[1].message
 
-  print(diagnostic_mesgage)
-  return diagnostic_mesgage
+  if cursor_diagnostic and #cursor_diagnostic > 0 then
+    return cursor_diagnostic[1].message
+  else
+    return ''
+  end
 end
 
 M.yank_diag = function()
-  get_cursor_diagnostic_message()
+  local diagnostic_message = get_cursor_diagnostic_message()
 end
 
 return M
