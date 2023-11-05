fb13e27f7f335979ebd1d6f618ed13361ab21ebd
suba <84484832+suba327777@users.noreply.github.com>
2023-11-05 17:38:57 +0900

    remove:setup and keymap fn

--- lua/yank_diag/init.lua
+++ lua/yank_diag/init.lua
@@ -2,20 +2,6 @@ local module = require("yank_diag.module")
 
 local M = {}
 
-M.config = {
-  mapping = "yd"
-}
-
-M.setup = function(args)
-  M.config = vim.tbl_deep_extend("force", M.config, args or {})
-  M.mapping()
-end
-
-M.mapping = function()
-  local mapping = "<leader>" .. M.config.mapping
-  vim.api.nvim_set_keymap("n", mapping, "<Cmd>YankDiag<CR>", { noremap = true, silent = true })
-end
-
 M.yank_diag = function()
   module.yank_diag()
 end
